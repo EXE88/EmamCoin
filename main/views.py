0f2fab40ecc757678e9aab31560cd49fc4f82163
EXE <bobolk1388@gmail.com>
2024-06-07 23:21:20 +0330

    rendering coin and addding click listener to coin image

--- main/views.py
+++ main/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.views import View
 
 class MineCoinPage(View):
+    template_name = "main/MineCoinPage.html"
     def get(self,request):
-        None
+        return render(request , self.template_name)
