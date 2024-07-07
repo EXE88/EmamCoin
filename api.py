426b0e738b84d54fbdfbbeb76ca1e5f1d582d190
EXE <bobolk1388@gmail.com>
2024-07-07 14:18:13 +0330

    can get user coin from database ,  user coins update , js coin counter

--- api.py
+++ api.py
@@ -17,7 +17,7 @@ def new_connection():
   return connection , cursor
 
 #user create route
-@app.route('/user/create/<string:username>/', methods=['POST'])
+@app.route('/user/create/<string:username>', methods=['POST'])
 def create_user(username):
     connection , cursor = new_connection()
     try:
@@ -29,6 +29,43 @@ def create_user(username):
     finally:
         cursor.close()
         connection.close()
+
+@app.route('/user/coin/add/<string:username>/<int:value>' , methods=['POST'])
+def add_coin(username,value):
+    connection , cursor = new_connection()
+    try:
+        user_id = cursor.execute(f"""
+                                 SELECT user_id 
+                                 FROM Users 
+                                 WHERE username="{username}"
+                                 """)
+        user_id = user_id.fetchone()
+        if user_id is not None:
+            cursor.execute(f"UPDATE Users SET coin = coin+{value} WHERE user_id={user_id[0]}")
+            connection.commit()
+            return jsonify({"info":"user coins updated successfully!"}) , 200
+        return jsonify({"error":"user not found!"}) , 404
+    except sqlite3.Error as error:
+        return jsonify({"error":error}) , 500
+    finally:
+        cursor.close()
+        connection.close()
+
+@app.route('/user/coin/get/<string:username>' , methods=['GET'])
+def get_coin_count(username):
+    connection , cursor = new_connection()
+    try:
+        coin_count = cursor.execute(f'SELECT coin FROM Users WHERE username="{username}"').fetchone()
+        if coin_count is not None:
+            return jsonify({'data':coin_count}) , 200
+        return jsonify({"error":"user not found"}) , 404
+    except sqlite3.Error as  error:
+        return jsonify({"error":error}) , 500
+    finally:
+        cursor.close()
+        connection.close()
+
+
     
 #run app
 if __name__=="__main__":
--- static/js/CoinCounterScript.js
+++ static/js/CoinCounterScript.js
@@ -1,4 +1,7 @@
 function add_coin_to_counter(){
-    console.log('hello');
-    //soon
+    const coin_counter_element = document.getElementById("coin-counter-text");
+    const coin_counter_str = coin_counter_element.textContent;
+    const coin_counter_int = parseInt(coin_counter_str);
+    const coin_counter_int_plus_one = coin_counter_int + 1;
+    coin_counter_element.textContent = coin_counter_int_plus_one;
 }
